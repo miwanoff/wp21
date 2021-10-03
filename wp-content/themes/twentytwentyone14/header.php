5bc90cc31ceaf1e0aa98260561aa9ce1f26cd6de
miwanoff <marina.iva.noffa@gmail.com>
2021-10-03 13:51:44 +0300

    globally changes

--- wp-content/themes/twentytwentyone14/header.php
+++ wp-content/themes/twentytwentyone14/header.php
@@ -13,24 +13,27 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<html <?php language_attributes();?> <?php twentytwentyone_the_html_classes();?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset');?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+<body <?php body_class();?>>
+    <?php wp_body_open();?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text"
+            href="#content"><?php esc_html_e('Skip to content', 'twentytwentyone');?></a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); 
-	// Вывод меню на главной странице.
+        <?php
+
+// Вывод меню на главной странице.
 if (is_front_page()) {
     if (has_nav_menu('header-menu')): ?>
-    <nav class="header-navigation" aria-label="<?php esc_attr_e('Header Menu', 'twentytwentyone');?>">
-        <?php
+        <nav class="header-navigation" aria-label="<?php esc_attr_e('Header Menu', 'twentytwentyone');?>">
+            <?php
 wp_nav_menu(
         array(
             'theme_location' => 'header-menu',
@@ -40,13 +43,15 @@ wp_nav_menu(
         )
     );
     ?>
-    </nav><!-- .footer-navigation -->
-    <?php endif;
+        </nav><!-- .footer-navigation -->
+        <?php endif;
 }
-	
-	?>
-	
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+get_template_part('template-parts/header/site-header');
+
+?>
+
+
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main" role="main">
